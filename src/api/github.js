44c71f76f50e6face06684402eae5dcee3765999
Thien Tran <thientran1707@gmail.com>
2016-11-01 03:14:03 +0800

    add api function format

---
+++ src/api/github.js
@@ -0,0 +1,21 @@
+import request from 'request-promise';
+
+export function fetchTeamContribution() {
+
+}
+
+export function fetchMemberCommitHistory() {
+
+}
+
+export function compareEfforts() {
+
+}
+
+export function fetchCommitHistoryWithCodeChunk() {
+
+}
+
+export function fetchTeamLinesOfCode() {
+
+}
